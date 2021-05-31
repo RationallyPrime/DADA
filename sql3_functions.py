499f0939c535044a38a1f38b2ee9c7949b89a8ac
RationallyPrime <rationallyprime@gmail.com>
2021-05-31 00:21:11 +0000

    Update sql3_functions.py

--- sql3_functions.py
+++ sql3_functions.py
@@ -2,7 +2,7 @@ import sqlite3
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
-con = sqlite3.connect("/Users/RationallyPrime/Downloads/2012_10.db")
+con = sqlite3.connect("Enter database here")
 
 mycursor = con.cursor()
 
@@ -18,17 +18,21 @@ extranonce = results[:,3]
 
 
 def hex_nonce(nonce):
-    hex_distribution = np.zeros((256,1))
+    hex_distribution = np.zeros((256))
+    lsb_values = []
     for s in nonce:
         lsb = int(s[0:2],16)
+        lsb_values.append(lsb)
         hex_distribution[lsb] += 1
 
-    return hex_distribution
+    return hex_distribution,lsb_values
 
-hex_distribution = hex_nonce(nonce)
-
+hex_distribution,hist_data = hex_nonce(nonce)
 hypothesis_sums = [sum(hex_distribution[0:5]),sum(hex_distribution[5:10]),sum(hex_distribution[19:24]),sum(hex_distribution[24:29]),sum(hex_distribution[29:34]),sum(hex_distribution[34:39]),sum(hex_distribution[39:44]),sum(hex_distribution[44:49]),sum(hex_distribution[49:54]),sum(hex_distribution[54:59])]
-
+plt.hist(hist_data,bins=128)
+plt.xlabel("LSB Value")
+plt.ylabel("Frequency")
+plt.show()
 
 r_1 = stats.binom_test(hypothesis_sums[1],hypothesis_sums[0]+hypothesis_sums[1], alternative='greater')
 r_2 = stats.binom_test(hypothesis_sums[3],hypothesis_sums[2]+hypothesis_sums[3],alternative='greater')
